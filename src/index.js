326590cf139d958f8ec5910f585b12d8c971e891
Ryan Bass <theryanbass@gmail.com>
2022-02-08 17:33:40 -0800

    seperate nested promises

--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import DinoSearch from './dino-search.js';
 import DinoInfo from './dino-info.js';
-// import *** from './js/*** */.js';
 
 $(document).ready(function() {
   $('#dinoSubmit').click(function() {
     $('#some-awesome-container').html("");
-    let promise = DinoSearch.getDino();
-    promise.then(function(response) {
-      let dinoSelector = JSON.parse(response);
-      document.querySelector('#some-awesome-container').innerHTML = dinoSelector;
-      let promiseAgain = DinoInfo.getInfo();
-      promiseAgain.then(function(responseAgain) {
-        let dinoInformation = JSON.parse(responseAgain);
-        for (let i = 0; i < dinoInformation.length; i++) {
+
+    Promise.all([DinoSearch.getDino(), DinoInfo.getInfo()])
+      .then(([result1, result2]) => {
+        console.log(result1, result2);
+        let dinoSelector = JSON.parse(result1);
+        let dinoInformation = JSON.parse(result2);
+
+        document.querySelector('#some-awesome-container').innerHTML = dinoSelector;
+
+    for (let i = 0; i < dinoInformation.length; i++) {
           if (dinoInformation[i].Name === dinoSelector.toString()) {
             document.querySelector('#some-awesome-container-again').innerHTML = dinoInformation[i].Description;
             return;
@@ -24,27 +25,5 @@ $(document).ready(function() {
           document.querySelector('#some-awesome-container-again').innerHTML = "No description avaliable.";
         }
       });
-    });
   });
 });
-
-
-
-
-// var xhr = new XMLHttpRequest();
-
-//   xhr.open('GET', 'https://dinoipsum.com/api/?format=json&words=1&paragraphs=1');
-//   xhr.onload = function() {
-//     if (this.status === 200) {
-//       let dinoSelector = JSON.parse(this.response);
-//       document.querySelector('#some-awesome-container').innerHTML = dinoSelector;
-//     } else {
-//       console.log('Where did all the dinosaurs go?');
-//     }
-//   };
-//   xhr.send();
-
-//https://dinoipsum.com/api?key1=value1&key2=value2...
-// format: {json, html, text} output format (defaults to html)
-// words: the number of words per paragraph (defaults to 30)
-// paragraphs: the number of paragraphs (defaults to 10)
